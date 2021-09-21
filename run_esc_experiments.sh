765c5dc28edd2f6975794df847ec6e3e11930116
mrpep94 <leonardodpepino@gmail.com>
2021-09-21 14:36:36 -0300

    update

---
+++ run_esc_experiments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+
+export LC_NUMERIC="en_US.UTF-8"
+export PYTHONHASHSEED=1234
+
+#PEG-SWA pretraining
+#paiprun configs/main/esc50_training.yaml --output_path experiments/esc50/peg_swa --mods "global/experiment_name=esc_peg_swa&global/pretrained_ast=pretrained_models/peg-embeddings-swa.dnn"
+
+#ABS-SWA pretraining
+#paiprun configs/main/esc50_training.yaml --output_path experiments/esc50/abs_swa --mods "global/experiment_name=esc_abs_swa&global/pretrained_ast=pretrained_models/abs-pe-encoding-swa.dnn"
+
+#PEG-PLUS-ABS pretraining
+paiprun configs/main/esc50_training.yaml --output_path experiments/esc50/peg_plus_abs_swa --mods "global/experiment_name=esc_peg_plus_abs_swa&global/pretrained_ast=pretrained_models/peg-plus-abs-swa.dnn"
